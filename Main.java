be1c45c24fbb56958321279bf053e90a5f6af050
Artur Branco <arturfdb@gmail.com>
2023-05-02 22:33:50 -0300

    Implement avl balance.

--- Main.java
+++ Main.java
@@ -1,23 +1,127 @@
 public class Main {
 
+    public static Tree tree;
+
     public static void main(String[] args) {
-        Tree tree = new Tree();
-        tree.insert(20);
-        tree.insert(2);
-        tree.insert(30);
-        tree.insert(3);
-        tree.remove(20);
-        tree.insert(4);
-        tree.insert(5);
-        tree.insert(7);
-        tree.remove(4);
-        tree.insert(6);
-        tree.remove(30);
-        tree.remove(3);
-        tree.insert(-1);
-        tree.insert(-10);
-        tree.insert(1);
-        tree.remove(2);
+        reset();
+
+        addAndPrint(10);
+        addAndPrint(8);
+        addAndPrint(7);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(10);
+        addAndPrint(12);
+        addAndPrint(18);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(10);
+        addAndPrint(12);
+        addAndPrint(8);
+        addAndPrint(7);
+        addAndPrint(6);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(10);
+        addAndPrint(8);
+        addAndPrint(12);
+        addAndPrint(13);
+        addAndPrint(14);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(20);
+        addAndPrint(10);
+        addAndPrint(30);
+        addAndPrint(5);
+        addAndPrint(16);
+        addAndPrint(25);
+        addAndPrint(40);
+        addAndPrint(7);
+        addAndPrint(50);
+        addAndPrint(3);
+        addAndPrint(2);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(20);
+        addAndPrint(15);
+        addAndPrint(30);
+        addAndPrint(10);
+        addAndPrint(18);
+        addAndPrint(25);
+        addAndPrint(40);
+        addAndPrint(5);
+        addAndPrint(35);
+        addAndPrint(50);
+        addAndPrint(60);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(20);
+        addAndPrint(15);
+        addAndPrint(18);
+
+        printSeparator();
+        reset();
+
+        addAndPrint(20);
+        addAndPrint(25);
+        addAndPrint(22);
+
+
+        printSeparator();
+        reset();
+
+        addAndPrint(50);
+        addAndPrint(40);
+        addAndPrint(70);
+
+        addAndPrint(30);
+        addAndPrint(41);
+        addAndPrint(60);
+        addAndPrint(80);
+
+        addAndPrint(20);
+        addAndPrint(35);
+        addAndPrint(42);
+        addAndPrint(55);
+        addAndPrint(65);
+        addAndPrint(75);
+        addAndPrint(90);
+
+        addAndPrint(10);
+        addAndPrint(52);
+        addAndPrint(58);
+        addAndPrint(100);
+
+        addAndPrint(57);
+
+
+
+
+
+    }
+
+    public static void reset(){
+        tree = new Tree();
+    }
+
+    public static void printSeparator(){
+        System.out.println("-------------------------------------");
+    }
+
+    public static void addAndPrint(int num){
+        tree.insert(num);
         tree.printTreeState();
     }
 }
--- Node.java
+++ Node.java
@@ -2,9 +2,30 @@ public class Node {
     private int key;
     private Node left;
     private Node right;
+    private int height;
+    private Logger logger;
 
     public Node(int key){
         this.key = key;
+        this.left = null;
+        this.right = null;
+        this.height = 1;
+        this.logger = new Logger();
+    }
+
+    public static int getNodeHeight(Node node){
+        if(node == null){
+            return 0;
+        }
+        return node.getHeight();
+    }
+
+    public void updateHeight(){
+        int leftSubtreeHeight = Node.getNodeHeight(this.getLeft());
+        int rightSubtreeHeight = Node.getNodeHeight(this.getRight());
+        int height = Math.max(leftSubtreeHeight, rightSubtreeHeight) + 1;
+        logger.log("Updating height of node with key " + this.key + " from " + this.height + " to " + height + ".");
+        this.setHeight(height);
     }
 
     public int getKey() {
@@ -29,5 +50,13 @@ public class Node {
 
     public void setRight(Node right) {
         this.right = right;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
     }
 }
